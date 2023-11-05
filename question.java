5380e1f49b61e95c8c50d82df655fb39d8348e7e
Himanshu Tiwari <147228399+himanshuu2611@users.noreply.github.com>
2023-11-05 22:32:21 +0530

    Create question.java

---
+++ question.java
@@ -0,0 +1,12 @@
+// this is reference class or we can say class of question and answer 
+
+public class question {
+    String prompt;
+    String answer;
+
+    public question(String prompt , String answer){
+        this.prompt=prompt;
+        this.answer=answer;
+
+    }
+}
